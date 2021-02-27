c244e2160ed52002f01314d545330cc2137d0b6e
sarath143 <asarathraja95244@gmail.com>
2021-02-26 22:53:19 -0800

    fitst commit

---
+++ firstdayclass/src/firstdayclass/feb27.java
@@ -0,0 +1,16 @@
+package firstdayclass;
+
+public class feb27 {
+	private void first() {
+System.out.println("my fist");
+	}
+	private void second() {
+	System.out.println("second");	
+	}
+public static void main(String[] args) {
+	feb27 c = new feb27();
+	c.first();
+	c.second();
+	
+}
+}
